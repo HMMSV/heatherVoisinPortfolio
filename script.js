ca277f04369610345c615a9f88d70e47465c9b06
HV <heather.m.voisin@gmail.com>
2023-01-04 13:55:32 -0500

    workig on form

--- script.js
+++ script.js
@@ -18,3 +18,11 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+// form reset
+
+window.onbeforeunload = () => {
+  for(const form of document.getElementsByTagName('form')) {
+    form.reset();
+  }
+}
