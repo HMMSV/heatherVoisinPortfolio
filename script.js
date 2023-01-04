9b5c09929911d1beafce944c1a0de1ee88133e65
HV <heather.m.voisin@gmail.com>
2023-01-04 12:11:53 -0500

    added hamburger menu

---
+++ script.js
@@ -0,0 +1,20 @@
+//hamburger menu
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navMenu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".navLink");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu))
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
